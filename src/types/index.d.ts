2db0dfb4861d1d5c98faf3a11110030c113c5f12
THEALEXSAND <gohangaymer878@gmail.com>
2026-07-26 21:38:03 -0400

    Fix import path for TECHNOLOGIES in index.d.ts to use correct data source

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,14 +1,14 @@
-import { TECHNOLOGIES } from "@/mocks/technologies";
+import { TECHNOLOGIES } from "@/data/technologies-data";
 
 type Technology = typeof TECHNOLOGIES[keyof typeof TECHNOLOGIES];
 
 interface Project {
-    name: string;
-    description: string;
-    images: string[];
-    technologies: Technology[];
-    features: string[];
-    featured: boolean;
-    year: number;
-    duration: string;
+	name: string;
+	description: string;
+	images: string[];
+	technologies: Technology[];
+	features: string[];
+	featured: boolean;
+	year: number;
+	duration: string;
 }
